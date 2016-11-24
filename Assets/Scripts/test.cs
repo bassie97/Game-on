81e2665b3ccb00e9dba3653cb32c3ed8144f3fd5
Bas de Ruiter <b.d.r._bas@live.nl>
2016-11-24 17:38:10 +0100

    initial commit

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    // Paul is lekker
+        //erg lekker
+        //I know boi
+        //juguyu
+        //testttt
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
