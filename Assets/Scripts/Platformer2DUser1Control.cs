50c85c56e343fb30f4497e19ba5c9e1a8a0a4dca
Bas de Ruiter <b.d.r._bas@live.nl>
2016-12-08 23:16:40 +0100

    fixed some problems after merge

--- Assets/Scripts/Platformer2DUser1Control.cs
+++ Assets/Scripts/Platformer2DUser1Control.cs
@@ -23,6 +23,7 @@ namespace UnityStandardAssets._2D
             {
                 // Read the jump input in Update so button presses aren't missed.
                 m_Jump = CrossPlatformInputManager.GetButtonDown("P1_Jump");
+                Debug.Log(m_Jump);
             }
         }
 
@@ -33,7 +34,7 @@ namespace UnityStandardAssets._2D
             bool crouch = Input.GetKey(KeyCode.LeftControl);
             float h = CrossPlatformInputManager.GetAxis("P1_Horizontal");
             // Pass all parameters to the character control script.
-            m_Character.Move(h, crouch, m_Jump);
+            m_Character.Move(h, m_Jump);
             m_Jump = false;
         }
     }
