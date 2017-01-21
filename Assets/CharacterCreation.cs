ac77b786bee2b65470a4ee82321303154cd377d2
Bas de Ruiter <b.d.r._bas@live.nl>
2017-01-21 15:17:26 +0100

    sounds and gender selection

---
+++ Assets/CharacterCreation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CharacterCreation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
