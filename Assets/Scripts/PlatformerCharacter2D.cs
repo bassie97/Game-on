e5b80738bbbd1a2683f93275f4303ecbb9dec33e
PaulKoning <paulkoning@live.nl>
2017-01-23 16:53:27 +0100

    Yes

--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -76,7 +76,7 @@ namespace UnityStandardAssets._2D
 
         private void OnTriggerEnter2D(Collider2D other){
 			if(other.CompareTag("PickUp")){
-				score = score + 5;
+                ammoCount = ammoCount + 1;
 				Destroy (other.gameObject);
 			}
             if (!invincible)
