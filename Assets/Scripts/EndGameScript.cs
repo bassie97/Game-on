87c8c3fcb9f5fbf00caf9b686dc2fc5a3f600b8a
Chris de Windt <chris_dewindt@hotmail.com>
2017-01-29 18:18:16 +0100

    Faster camera movement & new end scene

---
+++ Assets/Scripts/EndGameScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGameScript : MonoBehaviour {
+
+    [SerializeField]
+    private Text score;
+
+    // Use this for initialization
+    void Start()
+    {
+        //score.text = GameController.Instance.Score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+
+    public void EndGmae()
+    {
+        Application.Quit();
+    }
+}
