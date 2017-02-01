3bb920ae72824ad7a4047cac09cc9f5da7c4383a
Bas de Ruiter <b.d.r._bas@live.nl>
2017-02-01 16:44:27 +0100

    fixed sound

---
+++ Assets/GameSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class GameSound : MonoBehaviour
+{
+
+    public AudioClip sound;
+
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.Play();
+
+    }
+}
